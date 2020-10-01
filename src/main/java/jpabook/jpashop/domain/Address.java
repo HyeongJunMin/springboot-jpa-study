29bed804cfa64cbbe4e9c2c1baabf43590a14095
HyeongJunMin <alsgudwns007@naver.com>
2020-10-01 19:36:20 +0900

    강의 도메인분석설계 끝(엔티티 완료). 다음은 애플리케이션 구현 준비

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,13 +1,19 @@
 package jpabook.jpashop.domain;
 
 import javax.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Embeddable
-@Getter @Setter
+@Getter
+@AllArgsConstructor
 public class Address {
+
   private String city;
   private String street;
   private String zipCode;
+
+  protected Address() { }
+
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,14 +2,15 @@ package jpabook.jpashop.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.cache.spi.support.AbstractReadWriteAccess.Item;
 
 @Entity
 @Table(name = "order_item")
@@ -20,11 +21,11 @@ public class OrderItem {
   @Column(name = "order_item_id")
   private Long id;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "item_id")
   private Item item;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "order_id")
   private Order order;
 
